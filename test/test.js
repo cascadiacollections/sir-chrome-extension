3563fd577770c32f722778225dd524ab50736715
Kevin Coughlin <kevintcoughlin@gmail.com>
2016-04-24 12:23:00 -0700

    Bump Chrome manifest minor

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+const chrome = require('sinon-chrome');
+
+describe('sinon-chrome', function() {
+  it('has property runtime', function () {
+    expect(chrome).to.have.property('runtime');
+  });
+});
