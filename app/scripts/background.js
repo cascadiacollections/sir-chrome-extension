b8ceba229c20fc3fcdd88ece9910449208c394d0
Kevin Coughlin <kevintcoughlin@gmail.com>
2016-04-24 12:11:19 -0700

    ES6 syntax

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,7 +2,7 @@
  * S.I.R - Kevin Smith's SModcast Internet Radio
  * Unofficial Chrome Extension
  * @author       Kevin Coughlin <kevintcoughlin@gmail.com>
- * @copyright    2014 Kevin Coughlin
+ * @copyright    2014-2016 Kevin Coughlin
  * @license      {@link https://github.com/KevinTCoughlin/sir-chrome-extension/blob/master/LICENSE.md|MIT License}
  */
 (function() {
@@ -16,39 +16,32 @@
         badge: {
             text: 'OFF'
         },
-        toggleStream: _toggleStream,
-        init: _init,
-        handleBrowserAction: _handleBrowserAction
-    };
+        toggleStream: () => {
+          if (!SIR.player) {
+              SIR.init();
+          }
 
-    function _init() {
-        SIR.player = new Audio();
-        SIR.player.autoplay = true;
-        SIR.player.src = SIR.streamURL
-    };
-
-    function _toggleStream() {
-        if (!SIR.player) {
-            SIR.init();
+          if (SIR.player.paused) {
+              SIR.player.src = SIR.streamURL
+              SIR.player.play();
+              SIR.badge.text = "ON"
+          } else {
+              SIR.player.src = ''
+              SIR.player.pause();
+              SIR.badge.text = "OFF";
+          }
+        },
+        init: () => {
+          SIR.player = new Audio();
+          SIR.player.autoplay = true;
+          SIR.player.src = SIR.streamURL
+        },
+        handleBrowserAction: () => {
+          SIR.toggleStream();
+          chrome.browserAction.setBadgeText(SIR.badge);
         }
-
-        if (SIR.player.paused) {
-            SIR.player.src = SIR.streamURL
-            SIR.player.play();
-            SIR.badge.text = "ON"
-        } else {
-            SIR.player.src = ''
-            SIR.player.pause();
-            SIR.badge.text = "OFF";
-        }
-    }
-
-    function _handleBrowserAction() {
-        SIR.toggleStream();
-        chrome.browserAction.setBadgeText(SIR.badge);
-    }
+    };
 
     chrome.browserAction.setBadgeText(SIR.badge);
     chrome.browserAction.onClicked.addListener(SIR.handleBrowserAction);
-
 })();
