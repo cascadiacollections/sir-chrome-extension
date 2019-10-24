4479230bdf53935eddcaf272949ef14569b76c45
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-10-23 22:40:04 -0700

    Fix lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 // @ts-check
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
+  mode: process.env.NODE_ENV || 'development',
   devtool: 'inline-source-map',
   entry: './app/scripts/background.ts',
   output: {
-    path: path.join(__dirname, "app", "dist"),
+    path: path.join(__dirname, 'app', 'dist'),
     filename: 'background.js'
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js']
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      { test: /\.tsx?$/, loader: 'ts-loader' }
     ]
   },
   plugins: []
