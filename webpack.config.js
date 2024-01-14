4b8de71e20c0e697504f0b080416954b6ec117c6
Kevin Coughlin <706967+KevinTCoughlin@users.noreply.github.com>
2024-01-14 22:35:10 +0000

    Webpack 5 compat config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 // @ts-check
-const path = require('path')
-
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   devtool: 'inline-source-map',
   entry: './app/scripts/background.ts',
   output: {
-    path: path.join(__dirname, 'app', 'dist'),
     filename: 'background.js'
   },
   resolve: {
